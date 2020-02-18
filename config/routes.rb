04584d1097ec8e0c9d6655c7eb22867fc336750b
田中良征 <tanakaryousei@tanakayoshimasanoMacBook-Pro.local>
2020-02-18 12:44:26 +0900

    [finish]4-2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'blogs/index'
-  get 'blogs/show'
-  get 'blogs/new'
-  get 'blogs/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #4-2章の始まり
+  resources :blogs
 end

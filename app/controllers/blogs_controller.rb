92ef3c4985f1eb7e9197dc1d6d53484e94dbc5cb
田中良征 <tanakaryousei@tanakayoshimasanoMacBook-Pro.local>
2020-02-18 13:40:47 +0900

    [finish]4-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,11 +6,21 @@ class BlogsController < ApplicationController
   end
 
   def new
+  	@blog = Blog.new
   end
   #4-3章の始まり
   def create
+  	blog = Blog.new(blog_params)
+  	blog.ave
+  	redirect_to blogs_path
   end
 
   def edit
+  end
+
+  private
+
+  def blog_params
+  	params.require(:blog).permit(:title, :category, :body)
   end
 end
